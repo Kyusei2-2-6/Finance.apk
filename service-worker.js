e314a430ea8069de3a2cabadc6e313f563286ecb
Kyusei2-2-6 <maunyolongyaaaa@gmail.com>
2026-04-28 18:35:20 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,1 @@
-const CACHE = 'finance-final-v1';
-const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
-  self.skipWaiting();
-});
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
-});
-self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
-});
+self.addEventListener('fetch',e=>{})
